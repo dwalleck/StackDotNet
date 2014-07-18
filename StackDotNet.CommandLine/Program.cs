19125926a66fc15df361d36770559d32980ea3cb
Daryl Walleck <daryl.walleck@rackspace.com>
2014-07-17 23:12:18 -0500

    Adding basic volumes support

--- StackDotNet.CommandLine/Program.cs
+++ StackDotNet.CommandLine/Program.cs
@@ -21,14 +21,31 @@ namespace StackDotNet.CommandLine
             var access_response = identityClient.Authenticate(Resources.username, Resources.password, Resources.username);
             var access = access_response.Result;
             var computeClient = new ComputeClient(access.GetEndpoint("nova", "RegionOne").publicURL, access.token.id);
-            var servers = computeClient.ListServers().Result;
-            var server = computeClient.GetServer(servers[0].Id).Result;
-            Console.WriteLine(server);
+            var servers = computeClient.ListServersDetailed().Result;
+            var s = computeClient.GetServer(servers[0].Id).Result;
+            Console.WriteLine("Number of servers: " + servers.Count);
             var flavors = computeClient.ListFlavorsDetailed().Result;
             var images = computeClient.ListImagesDetailed().Result;
 
+            var totalRam = 0;
+            var totalDisk = 0;
+            var totalCPUs = 0;
+
+            foreach (Server server in servers)
+            {
+                var flavor = flavors.Where(f => f.Id == server.Flavor.Id).FirstOrDefault();
+                totalRam += flavor.Ram;
+                totalDisk += flavor.Disk;
+                totalCPUs += flavor.VCPUs;
+            }
+
+            Console.WriteLine("Ram used: " + totalRam);
+            Console.WriteLine("Disk used: " + totalDisk);
+            Console.WriteLine("CPUs used: " + totalCPUs);
+
             var blockStorageClient = new BlockStorageClient(access.GetEndpoint("cinder", "RegionOne").publicURL, access.token.id);
             var volumes = blockStorageClient.ListVolumes().Result;
+            Console.WriteLine("Number of volumes: " + volumes.Count);
             Console.WriteLine();
 
 
---
+++ StackDotNet/Clients/BlockStorageClient.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using StackDotNet.Models.BlockStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDotNet.Clients
+{
+    public class BlockStorageClient
+    {
+        public HttpClient Client { get; set; }
+        public string BaseUrl { get; set; }
+        public JsonSerializer Serializer { get; set; }
+
+        public BlockStorageClient(string baseUrl, string token)
+        {
+            Client = new HttpClient();
+            Client.DefaultRequestHeaders.Add("Accept", "application/json");
+            Client.DefaultRequestHeaders.Add("X-Auth-Token", token);
+            BaseUrl = baseUrl;
+        }
+
+        public async Task<List<Volume>> ListVolumes()
+        {
+            HttpResponseMessage response = await Client.GetAsync(BaseUrl + "/volumes");
+            var response_body = await response.Content.ReadAsStringAsync();
+            ListVolumesResponse volumes_response = JsonConvert.DeserializeObject<ListVolumesResponse>(response_body);
+            return volumes_response.Volumes;
+        }
+
+    }
+}
