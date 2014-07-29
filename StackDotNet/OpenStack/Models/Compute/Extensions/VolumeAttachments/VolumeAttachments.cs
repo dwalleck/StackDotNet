42084585a66a99771a8b6f252e9a30c34929a587
Daryl Walleck <daryl.walleck@rackspace.com>
2014-07-29 01:03:03 -0500

    Added volume attachments models

---
+++ StackDotNet/OpenStack/Models/Compute/Extensions/VolumeAttachments/VolumeAttachments.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDotNet.OpenStack.Models.Compute.Extensions.VolumeAttachments
+{
+    
+    public class ListVolumeAttachmentsResponse
+    {
+        [JsonProperty(PropertyName = "volumeAttachments")]
+        public List<VolumeAttachment> VolumeAttachments { get; set; }
+    }
+
+    public class VolumeAttachment
+    {
+        [JsonProperty(PropertyName = "device")]
+        public string Device { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "serverId")]
+        public string ServerId { get; set; }
+
+        [JsonProperty(PropertyName = "volumeId")]
+        public string VolumeId { get; set; }
+    }
+
+}
