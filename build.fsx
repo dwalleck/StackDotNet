0c7ec832de3e7e0733c8bd3c49d5a259fc050358
Daryl Walleck <daryl.walleck@rackspace.com>
2015-02-01 16:14:02 -0600

    Updating build number

--- build.fsx
+++ build.fsx
@@ -10,7 +10,7 @@ RestorePackages()
 let projectName = "StackDotNet"
 let projectDescription = "A C# SDK for interacting with OpenStack clouds."
 let projectSummary = projectDescription // TODO: write a summary
-let version = "0.2.0"
+let version = "0.2.1"
 let authors = ["Daryl Walleck"]
 
 // directories
